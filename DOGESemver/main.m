6808e67ced4f7da946376c67484186b2df4a7b12
vectorliu(刘天扬) <vectorliu@tencent.com>
2016-12-07 15:32:31 +0800

    Initial Commit

---
+++ DOGESemver/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DOGESemver
+//
+//  Created by vectorliu on 2016/12/7.
+//  Copyright © 2016年 DOGE Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
